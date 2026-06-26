b8ae135caa8858b07f4c6b7ce7777d75c4bd7b03
Aarti Dorale <aartidorle2004@gmail.com>
2026-06-26 19:00:39 +0530

    alter, update, describe, drop

--- employee sql.sql
+++ employee sql.sql
@@ -33,3 +33,25 @@ insert into employees5(eid, ename, dob, designation, joinDate, city, address, ge
 (3, 'Rahul', '1999-08-15', 'Trainer', '2026-04-10', 'Mumbai', 'Andheri', 'male', '9876543210', 'rahul@gmail.com', 60000.00, 3),
 (4, 'Sneha', '2001-01-20', 'Counselor', '2026-05-01', 'Nashik', 'College Road', 'female', '9876543211', 'sneha@gmail.com', 45000.00, 4),
 (5, 'Amit', '1998-12-10', 'Manager', '2026-06-15', 'Nagpur', 'Residential School', 'male', '9876543212', 'amit@gmail.com', 70000.00, 5);
+
+select * from employees5;
+
+alter table employees5 rename column contactNumber to phoneNo;
+
+alter table employees5 add column Age int;
+
+alter table employees5 drop column Age;
+
+describe employees5; --  to know datatype
+
+alter table employees5 modify column designation varchar(101);
+
+alter table employees5 modify column salary int check(salary>0);
+
+update employees5 set designation= "abc" where eid=2;
+
+update employees5 set Age = datediff(curDate(),dob)/365;
+
+set sql_safe_updates=0; #safe mode off 
+
+
